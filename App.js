84dc48f55832f72639278303a33f459375b86c03
iamdiluxedbutcooler <iamdiluxedbutcooler@gmail.com>
2021-10-03 05:10:20 +0700

    Update App.js

--- App.js
+++ App.js
@@ -1,15 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useState, useEffect} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-// import { Text } from 'react-native';
+import React, { useState } from 'react';
+import { LogBox, SafeAreaView } from 'react-native';
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { FontAwesome } from '@expo/vector-icons';
 import { auth } from './firebase';
+
+import ShopmindersTab from './screens/ShopmindersTab';
+import SettingsTab from './screens/SettingsTab';
+
 import LoginScreen from './screens/sessions/LoginScreen';
+import RegisterScreen from './screens/sessions/RegisterScreen';
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+// LogBox.ignoreLogs(['Setting a timer for a long period of time']);
 
 export default function App() {
   const [signedIn, setSignedIn] = useState(false);
+
   auth.onAuthStateChanged((user) => {
     if (user) {
       setSignedIn(true);
@@ -17,51 +28,103 @@ export default function App() {
       setSignedIn(false);
     }
   });
-  
-  /* return (
-    <NavigationContainer theme={DefaultTheme}>
-      {signedIn
-        ? (
-          <Text>Signed in</Text>
-        ) : (
-          <>
-            <StatusBar style="dark" />
-            <Stack.Navigator
+
+  const forFade = ({ current }) => ({
+    cardStyle: {
+      opacity: current.progress,
+      backgroundColor: 'transparent',
+    },
+  });
+
+  return (
+      <NavigationContainer theme={DefaultTheme}>
+        {signedIn
+          ? (
+            <SafeAreaView style={{flex: 1, backgroundColor: '#29434e'}}>
+              <Tab.Navigator
+                screenOptions={({ route }) => ({
+                  tabBarIcon: ({ color, size }) => {
+                    if (route.name === 'shopminders') {
+                      return (
+                        <FontAwesome 
+                        name='list-ul'
+                        size={size}
+                        color={color}
+                        />
+                      )
+                    }
+                    if (route.name === 'settings') {
+                      return (
+                        <FontAwesome 
+                          name="cogs"
+                          size={size}
+                          color={color}
+                        />
+                      )
+                    }
+                  },
+                })}
+                tabBarOptions={{
+                  activeTintColor: 'white',
+                  inactiveTintColor: '#819ca9',
+                  style: {
+                    backgroundColor: '#29434e'
+                  }
+                }}
+              >
+                <Tab.Screen 
+                  name="shopminders"
+                  component={ShopmindersTab}
+                  options={{
+                    title: 'Shopminders'
+                  }}
+                />
+                <Tab.Screen 
+                  name="settings"
+                  component={SettingsTab}
+                  options={{
+                    title: 'Settings'
+                  }}
+                />
+              </Tab.Navigator>
+            </SafeAreaView>
+          ) : (
+            <>
+              <StatusBar style="light" />
+              <Stack.Navigator
               mode="card"
               screenOptions={{
               }}
-            >
-              <Stack.Screen
-                name="signIn"
-                component={LoginScreen}
-                options={{
-                  title: 'Sign in : Pleng 11/08/64',
-                  headerStyle: {
-                    backgroundColor: '#29434e',
-                    borderBottomColor: '#29434e',
-                  },
-                  headerTintColor: '#fff',
-                }}
-              />
-            </Stack.Navigator>
-          </>
-        )}
-    </NavigationContainer>
-  );*/
-
-  return (
-    <View style={styles.container}>
-      {signedIn
-        ? (<Text>Pleng signed In</Text>) : (<Text>Pleng not signed in</Text>)
-      }
-    </View>
+              >
+                <Stack.Screen
+                  name="signIn"
+                  component={LoginScreen}
+                  options={{
+                    title: 'Sign in',
+                    headerStyle: {
+                      backgroundColor: '#29434e',
+                      borderBottomColor: '#29434e',
+                    },
+                    cardStyleInterpolator: forFade,
+                    headerTintColor: '#fff',
+                  }}
+                />
+                <Stack.Screen
+                  name="register"
+                  component={RegisterScreen}
+                  options={{
+                    title: 'Register',
+                    headerStyle: {
+                      backgroundColor: '#29434e',
+                      borderBottomColor: '#29434e',
+                    },
+                    cardStyleInterpolator: forFade,
+                    headerTintColor: '#fff',
+                  }}
+                />
+              </Stack.Navigator>
+            </>
+          )}
+      </NavigationContainer>
   );
 }
-const styles = StyleSheet.create({
-  container: {
-  flex: 1,
-  backgroundColor: '#fff',
-  alignItems: 'center',
-  justifyContent: 'center',
-  },
-  });
